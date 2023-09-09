2db6036b64624e1e6f98363c18c71bf574b63a26
Daniel Brown <94daniel.brown@gmail.com>
2023-09-09 21:56:03 +0100

    fizzbuzz

--- fizzbuzz/index.ts
+++ fizzbuzz/index.ts
@@ -11,24 +11,27 @@
 //   fizz
 //   4
 //   buzz
-var fizzBuzz = function (n) {
-    for (var i = 1; i <= n; i++) {
+
+const fizzBuzz = (n) => {
+    for (let i=1; i<=n; i++) {
         if (i % 15 === 0) {
-            console.log("fizzbuzz");
+            console.log("fizzbuzz")
         }
         else if (i % 3 === 0) {
-            console.log("fizz");
-        }
-        else if (i % 5 === 0) {
-            console.log("buzz");
-        }
-        else {
-            console.log(i);
+            console.log("fizz")
+        } else if (i % 5 === 0){
+            console.log("buzz")
+        } else {
+            console.log(i)
         }
     }
-};
-var app = function () {
+}
+
+const app = () => {
     console.log(fizzBuzz(25));
-};
+
+}
+
 app();
+
 module.exports = fizzBuzz;
