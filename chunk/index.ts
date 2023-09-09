2b765178448134aaeffde125207d27083f4fded5
Daniel Brown <94daniel.brown@gmail.com>
2023-09-09 23:38:12 +0100

    chunk

--- chunk/index.ts
+++ chunk/index.ts
@@ -7,20 +7,26 @@
 // chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
-var chunk = function (array, size) {
-    var n = (array.length / size);
-    var x = 0;
-    var newArray = [];
-    console.log(n);
-    for (var i = 0; i < array.length; i += size) {
-        newArray[x] = array.slice(i, i + size);
-        console.log(newArray);
+
+const chunk = (array, size) => {
+    const n: number = (array.length / size)
+    let x: number = 0;
+    const newArray = [];
+
+    console.log(n)
+    for (let i=0; i < array.length; i += size){
+        newArray[x] = array.slice(i, i + size)
+        console.log(newArray)
         x += 1;
     }
+
     return newArray;
-};
-var app = function () {
-    console.log(chunk([1, 2, 3, 4], 2));
-};
+}
+
+const app = () => {
+    console.log(chunk([1,2,3,4], 2))
+}
+
 app();
+
 module.exports = chunk;
