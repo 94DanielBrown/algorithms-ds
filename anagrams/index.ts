4bd533615a97943735ade58e008edbd385f173e2
Daniel Brown <94daniel.brown@gmail.com>
2023-09-24 12:40:37 +0100

    working anagram without spaces and capitals

--- anagrams/index.ts
+++ anagrams/index.ts
@@ -8,6 +8,30 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-const anagrams = (stringA: string, stringB: string) => {}
+const anagrams = (stringA: string, stringB: string): boolean => {
+    const charArrayA = stringA.split("");
+    const charArrayB = stringB.split("");
+    console.log(charArrayA)
+
+    for (let char of charArrayA) {
+        console.log(char)
+       if (charArrayB.includes(char)) {
+           console.log("found character")
+           let index = charArrayB.indexOf(char)
+           charArrayB.splice(index,1)
+       } else {
+           return false
+       }
+    }
+
+    return true
+}
+
+const app = () => {
+    console.log(anagrams("railsafety", "fairytales"))
+    console.log(anagrams("rail  safety", "fairy tales"))
+}
+
+app();
 
 module.exports = anagrams;
--- chunk/index.ts
+++ chunk/index.ts
@@ -25,6 +25,7 @@ const chunk = (array, size) => {
 
 const app = () => {
     console.log(chunk([1,2,3,4], 2))
+    console.log(chunk([1,2,3,4,5,6,7,8], 3))
 }
 
 app();
